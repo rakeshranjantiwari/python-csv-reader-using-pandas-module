232adb6d61102936f7b814d3e5a5365b835c000a
Rakesh Tiwari <rranjantiwari@gmail.com>
2022-11-28 13:01:29 +0530

    Add files via upload

---
+++ csv_reader.py
@@ -0,0 +1,5 @@
+import pandas
+#open the csv file
+#path should be full directory ex: /home/username/directory/filename.csv
+csv_file = pandas.read_csv('/home/rakesh/git-python/python-csv-reader-using-pandas-module/users.csv')
+print(csv_file)
