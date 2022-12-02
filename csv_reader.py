14bc238314d0e9fb7db3f0c2c70a7e66f42263ff
Rakesh Tiwari <rranjantiwari@gmail.com>
2022-12-02 12:59:58 +0530

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,5 +1,4 @@
 import pandas
 #open the csv file
-#path should be full directory ex: /home/username/directory/filename.csv
-csv_file = pandas.read_csv('add-your-full-file-path/users.csv')
+csv_file = pandas.read_csv('users.csv')
 print(csv_file)
